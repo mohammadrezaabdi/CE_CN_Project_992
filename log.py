6ba9c2bff0da22053f39b99a465a133b3141e3cc
ce424-992-97110422 <ce424-992-97110422@ce.tarasht.edu>
2021-07-15 13:33:52 +0430

    extracting server class

---
+++ log.py
@@ -0,0 +1,36 @@
+import logging
+
+LOG_DIR = '/home/ark/Documents/dev-null'
+loggers = ["manager", "client", "server", "group", "user", "channel"]
+STD_LOG_LEVEL = logging.DEBUG
+FILE_LOG_LEVEL = logging.INFO
+
+
+class LoggerFormatter(logging.Formatter):
+    name_just = 20
+    level_just = 15
+
+    def format(self, record):
+        time = self.formatTime(record, self.datefmt)
+        return f'===[{time}]===[{record.name}]'.ljust(self.name_just, "=") + \
+               f'===[{record.levelname}]==='.ljust(self.level_just, "=") + \
+               f' {record.getMessage()} :: ({record.filename}:{record.lineno})'
+
+
+default_format = LoggerFormatter()
+
+
+def init():
+    # setting logger
+    stdout_h = logging.StreamHandler()
+    filelg_h = logging.FileHandler(f"{LOG_DIR}/network.log")
+    stdout_h.setLevel(STD_LOG_LEVEL)
+    filelg_h.setLevel(FILE_LOG_LEVEL)
+    stdout_h.setFormatter(default_format)
+    filelg_h.setFormatter(default_format)
+
+    for logger_name in loggers:
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(STD_LOG_LEVEL)
+        logger.addHandler(stdout_h)
+        logger.addHandler(filelg_h)
--- manager.py
+++ manager.py
@@ -1,54 +1,15 @@
 import socket
-import threading
-import concurrent.futures
-import socket
 import logging
 import constants
+from server import Server
+
+import log
+
+log.init()
 
 nodes_port = []
+
 logger = logging.getLogger('manager')
-
-
-class Server:
-    def __init__(self, ip: str, port: str):
-        self.ip = ip
-        self.port = int(port)
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    def listen(self):
-        logger.info("server process started")
-        logger.info(f"trying to start listening at {self.ip}:{self.port}")
-        with self.sock as s:
-            s.bind((self.ip, self.port))
-            self.receive_clients()
-
-    def receive_clients(self):
-        self.sock.listen()
-        logger.info("started listening...")
-        with concurrent.futures.ThreadPoolExecutor() as thread_pool:
-            while True:
-                conn, addr = self.sock.accept()
-                logger.info(f"accepted new client with address {addr}")
-                thread_pool.submit(self.handle_client, conn)
-
-    @staticmethod
-    def handle_client(conn: socket.socket):
-        logger.debug("handling new client")
-        with conn:
-            try:
-                data = conn.recv(1024).decode("ascii")
-                logger.debug(f"received message is:{data}")
-                if not data:
-                    return
-
-                id, port = constants.CONNECT_REQUEST_REGEX.findall(data)
-                nodes_port.append((id, port))
-                parent_id, parent_port = get_parent(len(nodes_port) - 1)
-                response = constants.CONNECT_ACCEPT.format(id_parent=parent_id, port_parent=parent_port)
-                conn.sendall(response)
-
-            except Exception as e:
-                raise Exception(e)
 
 
 def get_parent(num):
@@ -67,8 +28,27 @@ ip = "127.0.0.1"
 port = "8080"
 
 
+def handle_client(conn: socket.socket):
+    logger.debug("handling new client")
+    with conn:
+        try:
+            data = conn.recv(1024).decode("ascii")
+            logger.debug(f"received message is:{data}")
+            if not data:
+                return
+
+            id, port = constants.CONNECT_REQUEST_REGEX.findall(data)
+            nodes_port.append((id, port))
+            parent_id, parent_port = nodes_port[get_parent(len(nodes_port) - 1)]
+            response = constants.CONNECT_ACCEPT.format(id_parent=parent_id, port_parent=parent_port)
+            conn.sendall(response)
+
+        except Exception as e:
+            raise Exception(e)
+
+
 def main():
-    manager = Server(ip, port)
+    manager = Server(ip, port,handle_client,logger)
     manager.listen()
 
 
