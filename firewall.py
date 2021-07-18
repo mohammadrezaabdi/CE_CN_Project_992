084b73833750953879baf5dbb465073cc0a1b1b3
mohammadrezaabdi <wnick2085@gmail.com>
2021-07-18 18:10:35 +0430

    better packaging

---
+++ firewall.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from enum import Enum
+import constants as consts
+from packet import PacketType
+
+
+class FWAction(Enum):
+    ACCEPT = 0
+    DROP = 1
+
+
+@dataclass
+class FWRule:
+    def __init__(self, src=consts.SEND_ALL, dst=consts.SEND_ALL, p_type=PacketType.ALL,
+                 action: FWAction = FWAction.DROP):
+        self.src = src
+        self.dst = dst
+        self.p_type = p_type
+        self.action = action
--- node.py
+++ node.py
@@ -3,10 +3,9 @@ import logging
 import socket
 import re
 import threading
-from dataclasses import dataclass
-from enum import Enum
+from firewall import FWAction, FWRule
 from chat import Chat, ChatState
-
+from table import IdTable
 import client
 import constants as consts
 import log
@@ -16,68 +15,6 @@ from server import Server
 
 log.init()
 logger = logging.getLogger("node")
-
-
-class FWAction(Enum):
-    ACCEPT = 0
-    DROP = 1
-
-
-@dataclass
-class IdRoute:
-    def __init__(self, dest: int, next_hop: tuple[int, int], state: FWAction = FWAction.ACCEPT):
-        self.dest = dest
-        self.next_hop = next_hop
-        self.state = state
-
-
-@dataclass
-class FWRule:
-    def __init__(self, src=consts.SEND_ALL, dst=consts.SEND_ALL, p_type=PacketType.ALL,
-                 action: FWAction = FWAction.DROP):
-        self.src = src
-        self.dst = dst
-        self.p_type = p_type
-        self.action = action
-
-
-class IdTable:
-    def __init__(self):
-        self.default_gateway: tuple[int, int] = tuple()
-        self.routing_table: list[IdRoute] = []
-        self.known_hosts: set[int] = set()
-        self.fw_rules: list[FWRule] = []
-
-    def get_next_hop(self, dest_id: int):  # todo Mahdi ghaznavi??
-        if dest_id not in self.known_hosts:
-            return consts.NEXT_HOP_NOT_FOUND
-        results = [route for route in self.routing_table if route.dest == dest_id and route.state == FWAction.ACCEPT]
-        if results:
-            return results[0].next_hop
-        return self.default_gateway
-
-    def add_entry(self, dest_id: int, next_hop: (int, int)):
-        results = [route.next_hop for route in self.routing_table if route.dest == dest_id]
-        if next_hop in results:
-            raise Exception("there is a loop")
-        self.routing_table.append(IdRoute(dest_id, next_hop))
-        self.known_hosts.add(dest_id)
-
-    def set_state(self, dest_id: int, state: FWAction):
-        for route in self.routing_table:
-            if route.dest == dest_id:
-                route.state = state
-
-    def fw_allows(self, p: Packet) -> bool:
-        src = p.src_id
-        dst = p.dest_id
-        p_type = p.p_type
-        # the & are for handling * in rules
-        rules = [rule for rule in self.fw_rules if
-                 rule.src & src == src and rule.dst & dst == dst and rule.p_type & p_type == p_type]
-        if rules:
-            return (True, False)[rules[-1].action == FWAction.DROP]
-        return True
 
 
 class Node:
